d4547f2f72b079abdf36d6db76c3af5afd422190
neil alden escobin <zz-az-devops-1@investagrams.com>
2023-08-03 07:04:19 +0800

    brought back redux persist

--- app/camper/profile/layout.tsx
+++ app/camper/profile/layout.tsx
@@ -1,23 +1,27 @@
-"use client";
-import SideNavigation from "@/components/SideNavigation";
-import TopNavigation from "@/components/TopNavigation";
-import { store } from "@/services/redux/store";
-import React from "react";
-import { Provider } from "react-redux";
-import styles from "./styles.module.css";
+"use client"
+import SideNavigation from '@/components/SideNavigation'
+import TopNavigation from '@/components/TopNavigation'
+import { persistor, store } from '@/services/redux/store'
+import React from 'react'
+import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react'
+import styles from "./styles.module.css"
 
 const Layout = ({ children }: any) => {
     return (
         <Provider store={store}>
-            <div className={styles.main}>
-                <TopNavigation />
-                <div className={styles.content}>
-                    <SideNavigation />
-                    <div className={styles.mainContent}>{children}</div>
+            <PersistGate loading={null} persistor={persistor}>
+                <div className={styles.main}>
+                    <TopNavigation />
+                    <div className={styles.content}>
+                        <SideNavigation />
+                        <div className={styles.mainContent}>{children}</div>
+                    </div>
                 </div>
-            </div>
-        </Provider>
-    );
-};
+            </PersistGate>
+        </Provider >
+    )
+}
 
-export default Layout;
+
+export default Layout
--- app/settings/retreatcenter/setupcamp/layout.tsx
+++ app/settings/retreatcenter/setupcamp/layout.tsx
@@ -1,21 +1,24 @@
 "use client"
 import SideNavigation from '@/components/SideNavigation'
 import TopNavigation from '@/components/TopNavigation'
-import { store } from '@/services/redux/store'
+import { persistor, store } from '@/services/redux/store'
 import React from 'react'
 import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react'
 import styles from "./styles.module.css"
 
 const Layout = ({ children }: any) => {
   return (
     <Provider store={store}>
-      <div className={styles.main}>
-        <TopNavigation />
-        <div className={styles.content}>
-          <SideNavigation />
-          <div className={styles.mainContent}>{children}</div>
+      <PersistGate loading={null} persistor={persistor}>
+        <div className={styles.main}>
+          <TopNavigation />
+          <div className={styles.content}>
+            <SideNavigation />
+            <div className={styles.mainContent}>{children}</div>
+          </div>
         </div>
-      </div>
+      </PersistGate>
     </Provider >
   )
 }
